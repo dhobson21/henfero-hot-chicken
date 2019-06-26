1f1f2c4b3fc2f2a04abd673c6a68c0795e47ebf3
Dustin Hobson <dhobson21@gmail.com>
2019-06-26 10:48:17 -0500

    refactor menu item fetch for better separationg of concerns

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,14 +1,13 @@
-import { makeMenuItemComponent, listMenuItems} from "./domstuff.js"
+
 
 //fetch menu_item data
 //Still need to get to main.js because babel only compiles main.js
-fetch("http://localhost:8088/menu_items")
-.then( menuData => menuData.json() )
-.then( menuItems => {
- //loop through menuItems and turn them into html components
-  let componentArray = menuItems.map( (oneMenuItemObj) => makeMenuItemComponent(oneMenuItemObj))
-  console.log(componentArray)
-  //add the menu item components to the DOM
-  listMenuItems(componentArray)
-})
 
+function getMenuItems () { //turn fetch call into function that returns fetch then do stuff to the data in main.js by calling the function
+return fetch("http://localhost:8088/menu_items")
+.then( menuData => menuData.json() )
+}
+
+
+
+export { getMenuItems }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,15 @@
-import "./api.js"
+import { getMenuItems } from "./api.js"
+import { makeMenuItemComponent, listMenuItems} from "./domstuff.js"
 
 
 console.log("this is main")
+
+getMenuItems()
+.then (menuItems => {
+console.log(menuItems)
+ //loop through menuItems and turn them into html components
+  let componentArray = menuItems.map( (oneMenuItemObj) => makeMenuItemComponent(oneMenuItemObj))
+  console.log(componentArray)
+  //add the menu item components to the DOM
+  listMenuItems(componentArray)
+})
